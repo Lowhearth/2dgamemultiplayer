1c1f8ddb89e2f1aa7b470ba3e809eb88e1e2df21
Me <me@Mes-MacBook-Pro.local>
2018-09-30 12:43:19 +0200

    port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
-serv.listen(2000);
+serv.listen(8080);
 
 var SOCKET_LIST = {}
 var PLAYERS_LIST = {}
